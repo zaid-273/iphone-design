e38c9188b3babcde2707bbe6e64183121fbabd3c
zaid-273 <zaidabukatab43@gmail.com>
2024-09-21 02:47:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img =document.querySelector('.img');
+let coteaner =document.querySelector('.coteaner');
+function phons(phon){
+    img.src =phon;
+}
+function colors(color){
+    coteaner.style.background = color;
+}
